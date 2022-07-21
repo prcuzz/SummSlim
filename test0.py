06cda6eb95964cefb0dd233e9099114e5e00d784
prcuzz <403124323@qq.com>
2022-07-21 17:13:22 +0800

    regular backup

--- test0.py
+++ test0.py
@@ -5,10 +5,20 @@ import time
 stderr_output = open("./stderr_output", "w")
 pid = os.popen('ps -ef | grep containerd ').readlines()[0].split()[1]
 print(pid)
-# p = subprocess.Popen(["strace", "-f", "-e", "trace=file", "-p", pid], stdout=stderr_file)
-p = subprocess.Popen(["strace", "-f", "-p", pid], stderr=stderr_output)
+
+p = subprocess.Popen(["strace", "-f", "-e", "trace=file", "-p", pid], stderr=stderr_output)
+# p = subprocess.Popen(["strace", "-f", "-p", pid], stderr=stderr_output)
+
+p1 = subprocess.Popen(["docker", "run", "--rm", "mongo"])
+time.sleep(1.5)
+p1.terminate()
+
 time.sleep(0.5)
 p.terminate()
+
 stderr_output.close()
 stderr_output = open("./stderr_output", "r")
-print(stderr_output.readlines())
+line = stderr_output.readline()
+while line:
+    print(line)
+    line = stderr_output.readline()
