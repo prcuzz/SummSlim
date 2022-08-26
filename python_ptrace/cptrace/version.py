5b0bb1cdc2e456fac6f6b91810cc821bcf90d92e
prcuzz <403124323@qq.com>
2022-08-26 17:02:03 +0800

    git rm some files

--- python_ptrace/cptrace/version.py
+++
@@ -1,4 +0,0 @@
-PACKAGE = "cptrace"
-VERSION = "0.6.1"
-WEBSITE = "http://python-ptrace.readthedocs.io/"
-LICENSE = "GNU GPL v2"
--- python_ptrace/ptrace/__init__.py
+++
@@ -1,3 +0,0 @@
-from .signames import SIGNAMES, signalName   # noqa
-from .error import PtraceError   # noqa
-from .version import VERSION, __version__  # noqa
